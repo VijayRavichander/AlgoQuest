a8ab01a31c3a2f19a7f113ec83e3dd9072bec1c6
Vijay Ravichander <vijaypreetham1@gmail.com>
2024-09-04 15:25:40 -0700

    Added MoneyTrees

---
+++ tle-3/moneyTrees.cpp
@@ -0,0 +1,107 @@
+// https://codeforces.com/problemset/problem/1873/F
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << "\n";
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve(){
+   int n, k;
+   cin>>n>>k;
+
+   vector<ll>f(n);
+   vector<ll>h(n);
+//    queue<int>q;
+   for(auto &it: f)cin>>it;
+   for(auto &it: h)cin>>it;
+   
+   int l = 0, r = 0;
+   int ans = 0;
+   int fruitSum = 0;
+   while(r < n){
+    if(f[r] > k){
+        fruitSum = 0;
+        l = r + 1;
+        r++;
+    }else{
+        if(l == r){
+            fruitSum += f[r];
+            ans = max(ans, r-l+1);
+            r++;
+        }else{
+            if(h[r-1] % h[r] == 0){
+                fruitSum += f[r];
+                while(fruitSum > k){
+                    fruitSum -= f[l];
+                    l++;
+                }
+                ans = max(ans, r-l+1);
+                r++;
+            }else{
+                fruitSum = 0;
+                l = r;
+            }
+            }   
+        }
+   }
+    cout<<ans<<endl;
+
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("Error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
